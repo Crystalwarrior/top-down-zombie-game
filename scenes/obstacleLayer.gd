8defac5a5aef1cf30d361b91d11308ed7f6ade12
Alex Noir <varsash@gmail.com>
2026-01-17 05:59:16 +0300

    Fix building new obstacles not properly updating the cells navigation obstacleLayer and obstacleBuild should have theur uids now

--- scenes/obstacleLayer.gd
+++ scenes/obstacleLayer.gd
@@ -2,6 +2,12 @@ extends TileMapLayer
 
 @onready var obstacles: TileMapLayer = $"../Obstacles"
 
+func _ready() -> void:
+	obstacles.cells_changed.connect(_on_obstacles_changed)
+
+func _on_obstacles_changed() -> void:
+	notify_runtime_tile_data_update()
+
 # Called when the node enters the scene tree for the first time.
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 	if coords in obstacles.get_used_cells_by_id(0):
--- scenes/obstaclebuild.gd
+++ scenes/obstaclebuild.gd
@@ -1,9 +1,11 @@
 extends TileMapLayer
 
+signal cells_changed
+
 #the Obstacle tilemap source ID (found in tilemaps window)
-@onready var tile_source_id: int = 0
+var tile_source_id: int = 0
 #the exact spot in which the tile is in the tileset. (Can also be checked in the tilemaps window)
-@onready var tile_atlas_coords: Vector2i = Vector2i(0,0)
+var tile_atlas_coords: Vector2i = Vector2i(0,0)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("build"):
@@ -11,3 +13,9 @@ func _input(event: InputEvent) -> void:
 		var local_pos = local_to_map(mouse_pos)
 		
 		set_cell(local_pos, 0, tile_atlas_coords)
+
+# according to https://github.com/godotengine/godot/issues/107342
+# the changed signal is not emitted when set_cell is called.
+# this is a workaround
+func _update_cells(_coords: Array[Vector2i], _forced_clean: bool) -> void:
+	cells_changed.emit()
